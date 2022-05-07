578a8957dc2248c109616a0d21e91476ae8e1628
Evgeny <sev.20@uni-dubna.ru>
2022-05-07 21:32:02 +0300

    [08] Move out hash funcs

--- 08_hash_table/hashtables.h
+++ 08_hash_table/hashtables.h
@@ -1,9 +1,18 @@
 #include "hashtable_base.h"
 #include <cmath>
 
+int hashfunc1(int key, uint size) { return key % size; }
+
+int hashfunc2(int key, uint size) {
+    const float GOLDEN_RATIO = (1 + sqrt(5)) / 2;
+
+    double stub;
+    return (int)(modf(key * GOLDEN_RATIO, &stub) * size);
+}
+
 class HashTable1 : public HashTableBase {
     private:
-        int hash(int key) override { return key % size; }
+        int hash(int key) override { return hashfunc1(key, size); }
 
     public:
         HashTable1(uint size) : HashTableBase(size) {}
@@ -12,12 +21,7 @@ class HashTable1 : public HashTableBase {
 
 class HashTable2 : public HashTableBase {
     private:
-        int hash(int key) override {
-            const float GOLDEN_RATIO = (1 + sqrt(5)) / 2;
-
-            double stub;
-            return (int)(modf(key * GOLDEN_RATIO, &stub) * size);
-        }
+        int hash(int key) override { return hashfunc2(key, size); }
 
     public:
         HashTable2(uint size) : HashTableBase(size) {}
