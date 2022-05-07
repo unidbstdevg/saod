1b5ad18090df0e683be5fccb441dc4815ef60e58
Evgeny <sev.20@uni-dubna.ru>
2022-05-07 21:52:49 +0300

    [08] Print statistics on tables fill

--- 08_hash_table/main.cpp
+++ 08_hash_table/main.cpp
@@ -1,8 +1,12 @@
 #include "hashtables.h"
+#include <chrono>
 #include <fstream>
 #include <iostream>
 using std::cout;
 using std::endl;
+using std::chrono::duration_cast;
+using std::chrono::high_resolution_clock;
+using std::chrono::milliseconds;
 
 HashTable1* hashtable1;
 HashTable2* hashtable2;
@@ -13,24 +17,40 @@ void init(uint size) {
     hashtable2 = new HashTable2(size);
 }
 
-bool fill_from_file(std::string filename) {
+template <typename T>
+void fill_table_from_file(std::string filename, T table) {
+    uint collisions_count = 0;
+
     std::ifstream file(filename);
+    auto start = high_resolution_clock::now();
     if(file.is_open()) {
         string line;
         while(getline(file, line)) {
             int key = stoi(line.substr(0, 5));
 
-            hashtable1->set(key, line);
-            hashtable2->set(key, line);
+            collisions_count += table->set(key, line);
         }
 
         file.close();
     } else {
         cout << "No such file: " << filename << endl;
-        return false;
+        exit(1);
     }
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start).count();
 
-    return true;
+    cout << "tcollisions: " << collisions_count << endl;
+    cout << "time: " << duration << " ms" << endl;
+}
+
+void fill_tables(std::string filename) {
+    cout << "Filling hashtable1" << endl;
+    fill_table_from_file(filename, hashtable1);
+    cout << endl;
+
+    cout << "Filling hashtable2" << endl;
+    fill_table_from_file(filename, hashtable1);
+    cout << endl;
 }
 
 int main(int argc, char* argv[]) {
@@ -53,8 +73,7 @@ int main(int argc, char* argv[]) {
     }
 
     init(size);
-    if(!fill_from_file(filename))
-        return 1;
+    fill_tables(filename);
 
     delete hashtable1;
     delete hashtable2;
